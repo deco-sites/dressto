9e78fd6e81c47dfa439276ab19530c1d6a5e5524
fernandocandeiatorres <fernandotorrescandeia@gmail.com>
2023-03-18 11:31:34 -0300

    highlights

--- components/ui/Highlights.tsx
+++ components/ui/Highlights.tsx
@@ -18,28 +18,34 @@ export interface Props {
 
 function Highlights({ highlights = [], title }: Props) {
   return (
-    <Container class="grid grid-cols-1 grid-rows-[48px_1fr] py-10">
+    <Container class="grid grid-cols-1 grid-rows-[48px_1fr] py-10 overflow-hidden">
       <h2 class="text-center">
         <Text variant="heading-2">{title}</Text>
       </h2>
 
       <Slider
-        class="gap-6"
+        class=""
         snap="snap-center sm:snap-start block first:ml-6 sm:first:ml-0 last:mr-6 sm:last:mr-0"
       >
         {highlights.map(({ href, src, alt, label }) => (
           <a
             href={href}
-            class="flex flex-col gap-4 items-center min-w-[190px]"
+            class="flex flex-col relative items-center min-w-[120px]"
           >
             <Image
-              class="rounded-[40px]"
+              class="rounded-full"
               src={src}
               alt={alt}
-              width={190}
-              height={265}
-            />
-            <Text variant="body">{label}</Text>
+              width={100}
+              height={100}
+            >
+              <Text
+                variant="body"
+                class="absolute pt-9 z-50 text-white font-thin text-lg tracking-widest"
+              >
+                PP
+              </Text>
+            </Image>
           </a>
         ))}
       </Slider>
