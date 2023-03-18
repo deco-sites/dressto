922d2714a30f3f0125d19bc7c13b35671c3a3c01
fernandocandeiatorres <fernandotorrescandeia@gmail.com>
2023-03-18 12:41:56 -0300

    added categories shelf and changed order of the images on the CMS

---
+++ sections/CategoriesShelf.tsx
@@ -0,0 +1,6 @@
+export { default } from "$store/components/ui/CategoriesShelf.tsx";
+export type {
+  Highlight,
+  Props,
+} from "$store/components/ui/CategoriesShelf.tsx";
+export type { Image as LiveImage } from "deco-sites/std/components/types.ts";
