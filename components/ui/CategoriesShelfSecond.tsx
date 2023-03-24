05d862ee1a1571c7dde959eae6d0001bad9774df
fernandocandeiatorres <fernandotorrescandeia@gmail.com>
2023-03-23 21:04:55 -0300

    fixing the size and display of the images on categories shelf second

--- components/ui/CategoriesShelfSecond.tsx
+++ components/ui/CategoriesShelfSecond.tsx
@@ -18,31 +18,81 @@ export interface Props {
 
 function CategoriesShelfSecond({ highlights = [], title }: Props) {
   return (
-    <Container class="grid grid-cols-1 grid-rows-[48px_1fr] py-10 overflow-hidden">
+    <Container class="grid grid-cols-1 grid-rows-[48px_1fr] min-w-full w-full py-10 overflow-hidden">
       <h2 class="text-center">
         <Text variant="heading-2">{title}</Text>
       </h2>
 
+      {/* Mobile Version mx-auto block gap-6 col-span-full row-start-2 row-end-5 */}
       <Slider
-        class="mx-auto block gap-6 col-span-full row-start-2 row-end-5"
+        class="lg:hidden block min-w-full w-full mx-4"
         snap="snap-center block sm:first:ml-0  sm:last:mr-0"
       >
         {highlights.map(({ href, src, alt, label }) => (
           <a
             href={href}
-            class="mx-auto p-4 bg-cover object-contain"
+            class=" p-4 bg-cover object-contain"
           >
             <Image
               class="object-contain overflow-hidden"
               src={src}
               alt={alt}
-              width={300}
-              height={600}
+              width={915}
+              height={1250}
             >
             </Image>
           </a>
         ))}
       </Slider>
+      {/* Desktop Version */}
+      <div class="hidden lg:flex flex-col min-w-full w-full">
+        <div class="flex flex-row mx-5 justify-between mb-32">
+          <a class="self-center mr-2" href={highlights[0].href}>
+            <Image
+              class="object-contain overflow-hidden"
+              src={highlights[0].src}
+              alt={highlights[0].alt}
+              width={600}
+              height={800}
+            >
+            </Image>
+          </a>
+          <a class="ml-2" href={highlights[1].href}>
+            <Image
+              class="object-contain overflow-hidden"
+              src={highlights[1].src}
+              alt={highlights[1].alt}
+              width={800}
+              height={1000}
+            >
+            </Image>
+          </a>
+        </div>
+        <div class="flex flex-row mx-auto justify-center min-w-full w-full mx-2 mb-40">
+          <a href={highlights[2].href}>
+            <Image
+              class="object-contain overflow-hidden"
+              src={highlights[2].src}
+              alt={highlights[2].alt}
+              width={1220}
+              height={670}
+            >
+            </Image>
+          </a>
+        </div>
+        <div class="flex flex-row mx-auto justify-center min-w-full w-full">
+          <a href={highlights[3].href}>
+            <Image
+              class="object-contain overflow-hidden"
+              src={highlights[3].src}
+              alt={highlights[3].alt}
+              width={1520}
+              height={634}
+            >
+            </Image>
+          </a>
+        </div>
+      </div>
     </Container>
   );
 }
